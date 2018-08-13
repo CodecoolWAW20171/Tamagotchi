8cf1fe78a59341fb02e973edaee143abba25e421
Pico143 <pico19942@gmail.com>
2018-08-13 12:41:39 +0200

    implemented functions for tamagotchi

--- src/com/codecool/tamagotchi/controller/GameController.java
+++ src/com/codecool/tamagotchi/controller/GameController.java
@@ -1,6 +1,11 @@
 package com.codecool.tamagotchi.controller;
 
+import com.codecool.tamagotchi.model.Tamagotchi;
+
 public class GameController {
+
+    Tamagotchi tamagotchi;
+
     public void initializeGame() {
 
     }
--- src/com/codecool/tamagotchi/model/Tamagotchi.java
+++ src/com/codecool/tamagotchi/model/Tamagotchi.java
@@ -1,5 +1,51 @@
 package com.codecool.tamagotchi.model;
 
 public class Tamagotchi {
+    private int Feeding;
+    private int Fun;
+    private int Age;
+
+    public Tamagotchi () {
+        Feeding = 100;
+        Fun = 100;
+        Age = 0;
+    }
+    public Tamagotchi (String filename) {
+
+    }
+
+    public void feed() {
+        this.Feeding = 100;
+    }
+
+    public void decreaseFeeding() {
+        this.Feeding -= 1;
+    }
+
+    public void haveFun() {
+        if (this.Fun < 80) {
+            this.Fun += 20;
+        }
+        else {
+            this.Fun = 100
+        }
+    }
+
+    public void getOlder() {
+        this.Age += 1;
+    }
+    
+    public int getFeeding(){
+        return this.Feeding;
+    }
+
+    public int getFun(){
+        return this.Fun;
+    }
+
+    public int getAge() {
+        return this.Age;
+    }
+
 
 }
