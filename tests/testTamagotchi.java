0bbf76d19ef012f61f72bae9fd7546a28d63a897
Pico143 <pico19942@gmail.com>
2018-08-16 10:55:07 +0200

    fixed tests a bit by adding before action to tests

--- tests/testTamagotchi.java
+++ tests/testTamagotchi.java
@@ -1,19 +1,25 @@
 import com.codecool.tamagotchi.model.Tamagotchi;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class testTamagotchi {
 
+    Tamagotchi tamagotchi;
+    @Before
+    public void initialize() {
+        this.tamagotchi = new Tamagotchi();
+    }
+
+
     @Test
     public void testFeeding() {
-        Tamagotchi tamagotchi = new Tamagotchi();
         tamagotchi.feed();
         Assert.assertEquals(100, tamagotchi.getFeeding());
     }
 
     @Test
     public void testDecreaseFeeding() {
-        Tamagotchi tamagotchi = new Tamagotchi();
         tamagotchi.feed();
         tamagotchi.decreaseFeeding();
         Assert.assertEquals(99, tamagotchi.getFeeding());
@@ -21,14 +27,12 @@ public class testTamagotchi {
 
     @Test
     public void haveFun() {
-        Tamagotchi tamagotchi = new Tamagotchi();
         tamagotchi.haveFun();
         Assert.assertEquals(100, tamagotchi.getFun());
     }
 
     @Test
     public void getOlder() {
-        Tamagotchi tamagotchi = new Tamagotchi();
         Assert.assertEquals(0, tamagotchi.getAge());
         tamagotchi.getOlder();
         Assert.assertEquals(1, tamagotchi.getAge());
