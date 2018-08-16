3c03eac62b37434c79e2cb5aa270282c09fbadbe
Pico143 <pico19942@gmail.com>
2018-08-16 11:32:16 +0200

    added binding to age, moved binding to separate file

--- src/com/codecool/tamagotchi/view/View.java
+++ src/com/codecool/tamagotchi/view/View.java
@@ -2,13 +2,17 @@ package com.codecool.tamagotchi.view;
 
 import com.codecool.tamagotchi.model.Tamagotchi;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Parent;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.converter.IntegerStringConverter;
+import javafx.util.converter.NumberStringConverter;
 
 
 public class View {
@@ -28,11 +32,15 @@ public class View {
     public void setUpBindings(Tamagotchi tamagotchi, Parent root) {
         ProgressBar feedingBar = (ProgressBar) root.lookup("#feedingBar");
         ProgressBar funBar = (ProgressBar) root.lookup("#funBar");
+        TextField ageField = (TextField) root.lookup("#ageField");
 
         ObservableValue<Integer> observableFeeding = new SimpleIntegerProperty(tamagotchi.getFeeding()).asObject();
         feedingBar.progressProperty().bindBidirectional((Property) observableFeeding);
 
         ObservableValue<Integer> observableFun = new SimpleIntegerProperty(tamagotchi.getFun()).asObject();
         funBar.progressProperty().bindBidirectional((Property) observableFun);
+
+        ObservableValue<Integer> observableAge = new SimpleIntegerProperty(tamagotchi.getAge()).asObject();
+        ageField.textProperty().bindBidirectional((Property) observableAge, new NumberStringConverter());
     }
 }
